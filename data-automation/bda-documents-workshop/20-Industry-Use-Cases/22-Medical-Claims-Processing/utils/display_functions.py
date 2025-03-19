4176e586176c4cd1d839c6117d5da4ca70fe16b9
Raja Vaidyanathan <rajavaid@amazon.co.uk>
2025-03-19 00:30:50 +0000

    sync gitlab

---
+++ data-automation/bda-documents-workshop/20-Industry-Use-Cases/22-Medical-Claims-Processing/utils/display_functions.py
@@ -0,0 +1,29 @@
+import ipywidgets as widgets
+from IPython.display import display, HTML
+import pandas as pd
+from PIL import Image
+import io
+import boto3
+import json
+from urllib.parse import urlparse
+
+
+s3 = boto3.client('s3')
+
+
+def get_view(data, display_function=None):
+    out = widgets.Output()
+    with out:
+        if callable(display_function):
+            display_function(data)
+        else:
+            display(data)
+    return out
+
+def display_multiple(views, view_titles = None):
+    main_tab = widgets.Tab()
+    for i, view in enumerate(views):
+        main_tab.children = (*main_tab.children, view)
+        tab_title = view_titles[i] if view_titles and view_titles[i] else f'Document {i}'
+        main_tab.set_title(i, title=tab_title)
+    display(main_tab)
